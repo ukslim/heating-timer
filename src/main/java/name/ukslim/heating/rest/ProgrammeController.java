f0c4880d3f87ab1bd6b2060c888611766cd78420
John Hartnup <john.hartnup@firstutility.com>
2017-03-09 15:22:52 +0000

    Use @RestController annotation

--- src/main/java/name/ukslim/heating/rest/ProgrammeController.java
+++ src/main/java/name/ukslim/heating/rest/ProgrammeController.java
@@ -1,21 +1,22 @@
 package name.ukslim.heating.rest;
 
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
 import name.ukslim.heating.timer.Programme;
 import name.ukslim.heating.types.Temperature;
 
-@Controller
+@RestController
 public class ProgrammeController {
 
     private final Programme programme;
@@ -25,8 +26,7 @@ public class ProgrammeController {
         this.programme = programme;
     }
     
-    @RequestMapping("heating/room/targetTemp")
-    @ResponseBody
+    @RequestMapping(method = GET, value = "heating/room/targetTemp")
     public Temperature temperature(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime) {
         return programme
                 .getTargetTemperature(dateTime)
